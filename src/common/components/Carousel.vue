16c57f4af0cd78c9f1340e18e4d98cc519f83dc5
王跃 <wangyue57@meituan.com>
2018-06-20 16:37:56 +0800

    轮播图

---
+++ src/common/components/Carousel.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="siema">
+    <div>Hi, I'm slide 1</div>
+    <div>Hi, I'm slide 2</div>
+    <div>Hi, I'm slide 3</div>
+    <div>Hi, I'm slide 4</div>
+  </div>
+</template>
+
+<script>
+  import Siema from 'siema'
+
+  export default {
+    name: 'Carousel',
+    props: {
+
+    },
+    mounted () {
+      new Siema({
+        selector: '.siema',
+        duration: 200,
+        easing: 'ease-out',
+        perPage: 1,
+        startIndex: 0,
+        draggable: true,
+        multipleDrag: true,
+        threshold: 20,
+        loop: false,
+        rtl: false,
+        onInit: () => {},
+        onChange: () => {},
+      })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
     <Cube :text="'测试'"/>
+    <Carousel/>
   </div>
 </template>
 
 <script>
   import { vuexMixin } from '../../common/mixins'
   import Cube from '../../common/components/Cube'
+  import Carousel from '../../common/components/Carousel'
 
   export default {
     name: 'Home',
     mixins: [vuexMixin],
-    components: {Cube},
+    components: {Cube, Carousel},
     data: () => ({}),
     methods: {}
   }
