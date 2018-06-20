badf4abeca1528f26b9c52d846cfa97ba88165be
皮娟 <trytry8090@163.com>
2018-06-20 16:37:36 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -49,4 +49,20 @@
   ul, li {
     list-style: none;
   }
+
+  .model {
+    background-color: rgba(0, 0, 0, 0.6);
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+
+  .center {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 </style>
