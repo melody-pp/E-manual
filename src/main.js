f650b5a0da1fcf32be0ab058a08179b9c52808ee
wangyue <hust_wangyue@foxmail.com>
2018-06-26 09:19:51 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import store from './store'
 
+if (!(navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+  location = '/e-manual-pc'
+}
 Vue.config.productionTip = false
 Vue.prototype.bus = new Vue()
 
